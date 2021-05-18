c4d66b9602be26e6e2d8ecea01d7c0c56dbfa70a
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-05-19 00:28:04 +0530

    Done with lexical analyser

--- lexical/33259_lexical.cpp
+++ lexical/33259_lexical.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Global tables to store data
 vector<string> us_table, symbol_table, literal_table, keyword_table, operator_table, double_operator_table;
 
+// Function declarations
 void add_data();
 void write_data();
 int is_operator(string);
@@ -17,9 +19,11 @@ int main()
 {
     ifstream input_file;
     string temp;
+
     // Adding keywords, operators from text files;
     add_data();
 
+    // Taking input from user and reading and processing it line by line
     input_file.open("input.txt");
 
     if (input_file.good())
@@ -37,11 +41,14 @@ int main()
 
     input_file.close();
 
+    // Writing data to the console
+
     write_data();
 
     return 0;
 }
 
+// Function populate keyword and operator tables
 void add_data()
 {
     string temp;
@@ -137,6 +144,7 @@ void write_data()
     cout << "-----------------------------------" << endl;
 }
 
+// Check if token passed operator
 int is_operator(string op)
 {
     for (int i = 0; i < operator_table.size(); i++)
@@ -151,6 +159,7 @@ int is_operator(string op)
     return -1;
 }
 
+// Check if token passed operator
 int is_double_operator(string op)
 {
     for (int i = 0; i < double_operator_table.size(); i++)
@@ -159,6 +168,7 @@ int is_double_operator(string op)
     return -1;
 }
 
+// Check if token passed is a keyword
 int is_keyword(string word)
 {
     for (int i = 0; i < keyword_table.size(); i++)
@@ -167,6 +177,7 @@ int is_keyword(string word)
     return -1;
 }
 
+// Check if there is a space
 bool is_separator(char a)
 {
     if (a == ' ')
@@ -174,6 +185,7 @@ bool is_separator(char a)
     return false;
 }
 
+// Check if token passed a literal
 bool is_literal(string word)
 {
     int flag = 0;
@@ -194,6 +206,7 @@ bool is_literal(string word)
     return false;
 }
 
+// Add data to UST after formatting tabs and spaces
 void add_to_ust(string lexeme, string type, int index, string token)
 {
     string temp = "";
@@ -210,15 +223,19 @@ void add_to_ust(string lexeme, string type, int index, string token)
 void process_line(string line)
 {
     // Dealing with comments
-
     if (line[0] == '/' && line[1] == '/')
         return;
+
+    // Initialisation
     string temp = "";
     int check;
+
+    // Iterating line character by character
     for (int i = 0; i < line.length(); i++)
     {
         temp.append(string(1, line[i]));
 
+        // Dealing with double operator
         if (i > 0)
         {
             check = is_double_operator(line.substr(i - 1, 2));
@@ -273,6 +290,8 @@ void process_line(string line)
                 temp = "";
             }
         }
+
+        // Dealing with operator
         check = is_operator(string(1, line[i]));
 
         if (check != -1)
@@ -342,6 +361,8 @@ void process_line(string line)
             }
             temp = "";
         }
+
+        // Checking for spaces
         else if (is_separator(line[i]))
         {
             if (temp.length() > 1)
@@ -365,12 +386,14 @@ void process_line(string line)
             }
             temp = "";
         }
+        // Checking for keywords
         else if (is_keyword(temp) != -1)
         {
             check = is_keyword(temp);
             add_to_ust(temp, "KEYW", check, temp);
             temp = "";
         }
+        // Checking for literals
         else if (is_literal(temp))
         {
             while (is_literal(temp))
@@ -383,3 +406,65 @@ void process_line(string line)
         }
     }
 }
+
+/* Output - 
+
+tanmay@Predator:~/Code/SP/lexical$ g++ 33259_lexical.cpp 
+tanmay@Predator:~/Code/SP/lexical$ ./a.out 
+Keywords added
+Operators added
+---------------------Universal Symbol table----------------------------
+LEXEME          TYPE            INDEX           TOKEN
+-----------------------------------------------------------------------
+(               TERM            15              (
+main            SYM             0               ID
+)               TERM            16              )
+{               TERM            13              {
+int             KEYW            16              int
+,               TERM            0               ,
+a               SYM             1               ID
+;               TERM            1               ;
+b               SYM             2               ID
+a               SYM             1               ID
+++              TERM            0               ++
+;               TERM            1               ;
+int             KEYW            16              int
+;               TERM            1               ;
+c               SYM             3               ID
+c               SYM             3               ID
+=               TERM            2               =
+a               SYM             1               ID
++               TERM            5               +
+;               TERM            1               ;
+b               SYM             2               ID
+(               TERM            15              (
+printf          SYM             4               ID
+)               TERM            16              )
+"Hello"         LIT             0               CONST
+;               TERM            1               ;
+return          KEYW            19              return
+0               LIT             1               CONST
+}               TERM            14              }
+-----------------------------------------------------------------------
+
+
+----------Symbol table-------------
+INDEX   SYMBOL
+-----------------------------------
+0       main
+1       a
+2       b
+3       c
+4       printf
+-----------------------------------
+
+
+----------Literal table-------------
+INDEX   LITERAL
+------------------------------------
+0       "Hello"
+1       0
+-----------------------------------
+tanmay@Predator:~/Code/SP/lexical$ 
+
+*/

c45ea4d3f97232a5cc80eed1c5231bbcd9fafc44
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-05-20 15:17:10 +0530

    RDP

--- rdp/rdp.cpp
+++ rdp/rdp.cpp
@@ -1,22 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+Grammar : 
+A -> aAa | bBb
+B -> bBb | NULL
+*/
+
+void A();
+void B();
+
 string s;
 int ptr = 0;
+
+int main()
+{
+    cout << "Enter a string(Please end the string with a $):- ";
+    cin >> s;
+
+    A();
+
+    if (s[ptr] == '$')
+    {
+        cout << "String " << s << " accepted" << endl;
+    }
+    else
+    {
+        cout << "String " << s << " rejected" << endl;
+    }
+    return 0;
+}
 
 void A()
 {
     if (s[ptr] == 'a')
+    {
         ptr++;
-    A();
-    if (s[ptr] == 'a')
+        A();
+        if (s[ptr] == 'a')
+            ptr++;
+        else
+            return;
+    }
+    if (s[ptr] == 'b')
+    {
         ptr++;
+        B();
+        if (s[ptr] == 'b')
+            ptr++;
+        else
+            return;
+    }
     else
         return;
 }
 
-int main()
+void B()
 {
-
-    cin >> s;
+    if (s[ptr] == 'b')
+    {
+        ptr++;
+        B();
+        if (s[ptr] == 'b')
+            ptr++;
+        else
+            return;
+    }
 }
